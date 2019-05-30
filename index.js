036f81d5c0e5917628dc98582f2f169edfc1cde6
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-30 22:04:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ function displayRepositories() {
   const html =
     '<ul>' +
     repos.map(function(repo){
-      debugger
       return `<li><h2> ${repo.owner.login} </h2>
       <a href="${repo.html_url}" data-repo="${repo.name}"> ${repo.name} </a><br>
       <a href="#" onclick="getCommits(this)">Get Commits</a><br>
