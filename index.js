3999ea524a726203c65301f6578365d5d609b2a5
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-30 22:45:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,7 +31,20 @@ function displayRepositories() {
 }
 
 function displayBranches() {
-
+  const branches = JSON.parse(this.responseText)
+  const html =
+    '<ul>' +
+    branches.map(function(repo){
+      debugger
+      return `<li><h2> ${repo.owner.login} </h2>
+      <a href="${repo.html_url}" data-repo="${repo.name}"> ${repo.name} </a><br>
+      <a href="#" onclick="getCommits(this)">Get Commits</a><br>
+      <a href="#" onclick="getBranches(this)">Get Branches</a><br>
+      `
+    }).join('')
+    +
+    '</ul>'
+  $('#repositories').html(html)
 }
 
 function getCommits(el){
