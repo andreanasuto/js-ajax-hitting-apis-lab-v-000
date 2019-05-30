d3ca69cb2a628bf4db723698fd735148c72af29b
Andrea Nasuto <andrea.nasuto@gmail.com>
2019-05-30 20:33:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function displayCommits() {
+  $('#repositories')
+}
+
+function displayRepositories() {
+  $('#repositories').html
+}
+
+function displayBranches() {
+
+}
+
+function getCommits(){
+  $('input[type="text"]').
+}
+
+function getBranches(){
+
+}
+
+function getRepositories(){
+
+}
